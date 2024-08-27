974125d285f774d9609379720f707f0302f4dc6b
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-08-26 21:23:30 -0300

    Podcast.java

--- Alura Desafio POO/Audio/src/modelos/Podcast.java
+++ Alura Desafio POO/Audio/src/modelos/Podcast.java
@@ -1,33 +1,19 @@
 package modelos;
 
-public class Podcast extends Audio{
+public class Podcast extends Audio {
     private int numeroDeEp;
     private int duracaoEp;
 
-    public Podcast(String nome, int numeroDeEp, int duracaoEp) {
-        setNome(nome);
+    public Podcast(String nome, int duracao, int totalVisu, int curtidas, int classificao, int numeroDeEp, int duracaoEp) {
+        super(nome, duracao, totalVisu, curtidas, classificao);
         this.numeroDeEp = numeroDeEp;
         this.duracaoEp = duracaoEp;
     }
-
-    public void setNumeroDeEp(int numeroDeEp) {
-        this.numeroDeEp = numeroDeEp;
-    }
-
-    public void setDuracaoEp(int duracaoEp) {
-        this.duracaoEp = duracaoEp;
-    }
-
     public int getNumeroDeEp() {
         return numeroDeEp;
     }
 
     public int getDuracaoEp() {
         return duracaoEp;
-    }
-
-    @Override
-    public int duracao(){
-        return numeroDeEp * duracaoEp;
     }
 }
