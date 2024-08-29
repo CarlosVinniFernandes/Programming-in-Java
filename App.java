dab5768574c3661b70c9f5b1f2d0f5d050df6ac3
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-08-28 21:55:17 -0300

    Add files via upload

--- App.java
+++ App.java
@@ -1,43 +1,12 @@
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Conversao c = new Conversao();
-        Scanner input = new Scanner(System.in);
-       
-        while (true) {
-            System.out.println("Escolha a opção de conversão:");
-            System.out.println("1. Celsius para Fahrenheit");
-            System.out.println("2. Fahrenheit para Celsius");
-            System.out.println("3. Celsius para Kelvin");
-            System.out.println("4. Kelvin para Celsius");
-            System.out.println("5. Celsius para Réaumur");
-            System.out.println("6. Réaumur para Celsius");
-            System.out.println("7. Kelvin para Rankine");
-            System.out.println("8. Rankine para Kelvin");
-            System.out.println("9. Sair");
+        System.out.println("Hello, World!");
+        
+        ControleRemoto c1 = new ControleRemoto(50, true, true);
 
-            int escolha = input.nextInt();
-
-            if (escolha == 1){
-                System.out.println("Digite a temperatura: ");
-                double temperatura = input.nextDouble();
-
-                c.celsiusFahrenheit(temperatura);
-                System.out.println(c.celsiusFahrenheit(temperatura));
-            }
-            
-            if (escolha == 9) {
-                System.out.println("Saindo...");
-                break;
-            }
-
-            if (escolha < 1 || escolha > 9) {
-                System.out.println("Escolha inválida, tente novamente.");
-                continue;
-            }
-
-            input.close();
-        }
+        c1.abrirMenu();
+        c1.maisVolume();
+        c1.maisVolume();
+        c1.abrirMenu();
     }
 }
