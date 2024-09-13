44d24734db6701f5c2b0fb24cd30a7800e9dbdc6
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-09-13 12:40:44 -0300

    Add files via upload

---
+++ Prova 13-09/Livro.java
@@ -0,0 +1,18 @@
+public class Livro {
+    String titulo;
+    String autor;
+    int numeroPaginas;
+    String categoria;
+    public String nome;
+   
+    public Livro(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Livro(){
+    }
+    
+    
+    
+
+}
