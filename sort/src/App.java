23220128fe706037fab0fbaedea6e423ef2f45bb
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2024-11-26 22:10:45 -0300

    Add files via upload

---
+++ sort/src/App.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class App {    
+        public static void main(String[] args) throws Exception {
+            int[] vetor = new int[5];
+
+            vetor[0] = 10;
+            vetor[1] = 5;
+
+            for(int i = 0; i < vetor.length; i++){
+                System.out.println(vetor[i]);
+            }
+    }
+}
