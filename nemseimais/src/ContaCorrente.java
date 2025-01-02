5d3981f917b8ee58826e0222db7d9555171a9ecc
Carlos Fernandes <161844750+CarlosVinniFernandes@users.noreply.github.com>
2025-01-01 23:36:04 -0300

    Add files via upload

---
+++ nemseimais/src/ContaCorrente.java
@@ -0,0 +1,11 @@
+public class ContaCorrente extends Conta{
+    
+    void atualiza(double taxa){
+        super.atualiza(taxa * 2);
+    }
+
+    void deposita(double valor) {
+        this.saldo += valor - 0.10;
+        }
+
+}
